f132001c435c61e36d6b3c99f0f52e910ae63ed2
Ponfa Michael Joseph <ponfa_m@outlook.com>
2024-10-30 16:17:51 +0100

    Collateral NFT Project

---
+++ README.md
@@ -0,0 +1,32 @@
+# NFT-Collaterized-Lending-Platform
+
+## Installation
+
+- Clone this repo
+- Install dependencies
+
+```bash
+$ yarn && forge update
+```
+
+### Compile
+
+```bash
+$ npx hardhat compile
+```
+
+## Deployment
+
+### Hardhat
+
+```bash
+$ npx hardhat run scripts/deploy.js
+```
+
+### Foundry
+
+```bash
+$ forge t
+```
+
+
---
+++ test/deployDiamond.t.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../contracts/interfaces/IDiamondCut.sol";
+import "../contracts/facets/DiamondCutFacet.sol";
+import "../contracts/facets/DiamondLoupeFacet.sol";
+import "../contracts/facets/OwnershipFacet.sol";
+import "../contracts/facets/NFTCollateralFacet.sol";
+import "../contracts/facets/LoanFacet.sol";
+import "../contracts/Diamond.sol";
+
+import "./helpers/DiamondUtils.sol";
+
+import "openzeppelin-contracts/contracts/token/ERC721";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+
+contract TestNft is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("TestNft", "TNFT")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
+contract DiamondDeployer is DiamondUtils, IDiamondCut {
+    //contract types of facets to be deployed
+    Diamond diamond;
+    DiamondCutFacet dCutFacet;
+    DiamondLoupeFacet dLoupe;
+    OwnershipFacet ownerF;
+    NFTCollateralFacet nftCltF;
+    LoanFacet loanF;
+
+    function testDeployDiamond() public {
+        //deploy facets
+        dCutFacet = new DiamondCutFacet();
+        diamond = new Diamond(address(this), address(dCutFacet));
+        dLoupe = new DiamondLoupeFacet();
+        ownerF = new OwnershipFacet();
+        nftCltF = new NFTCollateralFacet();
+        loanF = new LoanFacet();
+
+        //upgrade diamond with facets
+
+        //build cut struct
+        FacetCut[] memory cut = new FacetCut[](4);
+
+        cut[0] = (
+            FacetCut({
+                facetAddress: address(dLoupe),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("DiamondLoupeFacet")
+            })
+        );
+
+        cut[1] = (
+            FacetCut({
+                facetAddress: address(ownerF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("OwnershipFacet")
+            })
+        );
+
+        cut[2] = (
+            FacetCut({
+                facetAddress: address(nftCltF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("NFTCollateralFacet")
+            })
+        );
+
+        cut[3] = (
+            FacetCut({
+                facetAddress: address(loanF),
+                action: FacetCutAction.Add,
+                functionSelectors: generateSelectors("LoanFacet")
+            })
+        );
+
+        //upgrade diamond
+        IDiamondCut(address(diamond)).diamondCut(cut, address(0x0), "");
+
+        //call a function
+        DiamondLoupeFacet(address(diamond)).facetAddresses();
+    }
+
+    // tests for nft collateral and loan management features
+    function test_NFT_Collateral() public {}
+    
+    function test_Loan_Management() public {}
+
+    function diamondCut(
+        FacetCut[] calldata _diamondCut,
+        address _init,
+        bytes calldata _calldata
+    ) external override {}
+}
